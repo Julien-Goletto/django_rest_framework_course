5725982c9a2de114c1005fe2c944f3d706844880
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-05-19 12:28:50 +0200

    :rocket: Finished drf tutorial

--- backend/ecommerce/views.py
+++ backend/ecommerce/views.py
@@ -1,39 +1,59 @@
-from django.shortcuts import render
 from json import JSONDecodeError
 from django.http import JsonResponse
 from .serializers import ItemSerializer, OrderSerializer
-from .models import Item, Order
-from rest_framework.permissions import IsAuthenticated
+from .models import Item , Order
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import ListModelMixin,UpdateModelMixin,RetrieveModelMixin
 
-class ItemViewSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
-    # A simple ViewSet for listing or retrieving items.
-    permission_classes = IsAuthenticated
+
+
+class ItemViewSet(
+        ListModelMixin,
+        RetrieveModelMixin, 
+        viewsets.GenericViewSet
+        ):
+    """
+    A simple ViewSet for listing or retrieving items.
+    """
+    permission_classes = (IsAuthenticated,)
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
-class OrderViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, viewsets.GenericViewSet):
-    # A simple ViewSet for listing, retrieving or creating orders.
-    permission_classes = IsAuthenticated
+
+
+
+class OrderViewSet(
+        ListModelMixin,
+        RetrieveModelMixin,
+        UpdateModelMixin, 
+        viewsets.GenericViewSet
+        ):
+    """
+    A simple ViewSet for listing, retrieving and creating orders.
+    """
+    permission_classes = (IsAuthenticated,)
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        # Should return a list of all orders for the authenticated user
+        """
+        This view should return a list of all the orders
+        for the currently authenticated user.
+        """
         user = self.request.user
         return Order.objects.filter(user = user)
-    
+
     def create(self, request):
         try:
             data = JSONParser().parse(request)
-            serializer = OrderSerializer(data = data)
-            if serializer.is_valid(raise_exception = True):
+            serializer = OrderSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
                 item = Item.objects.get(pk = data["item"])
                 order = item.place_order(request.user, data["quantity"])
                 return Response(OrderSerializer(order).data)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except JSONDecodeError:
-            return JsonResponse({"result": "error", "message": "Json decoding error"}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
